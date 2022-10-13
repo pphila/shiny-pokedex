1f6fff11febc2ae95089aa4991baa1e5cccee3f4
pphila <efl2ice89@gmail.com>
2022-10-12 20:38:11 -0700

    add UI logic and css sytling
    co-authored-by Quin Asselin <quin.asselin1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import './css/styles.css';
 
+//Business Logic
 const fetchPokemon = () => {
-  for (let i = 1; i <= 150; i++) {
+  const promises = [];
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const pokemon = {
-          name: data.name,
-          id: data.id,
-          image: data.sprites['front_shiny'],
-          type: data.types.map((type) => type.type.name).join(', ')
-        };
-        console.log(pokemon);
-      });
+    promises.push(fetch(url).then((response) => response.json()));
   }
-};
 
+  Promise.all(promises).then( results => {
+    const pokemon = results.map((data) => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites['front_shiny'],
+      type: data.types.map((type) => type.type.name).join(', ')
+    }));
+    displayPokemon(pokemon);
+  });
+};
 fetchPokemon();
+
+//UI logic
+const pokedex = document.getElementById('pokedex');
+const displayPokemon = (pokemon) => {
+  const pokemonHTMLString = pokemon.map ( poke => `
+  <li class="card">
+    <img class="card-image" src="${poke.image}"/>
+    <h2 class="card-title"> ${poke.id}. ${poke.name}</h2>
+    <p class="card-subtitle">Type: ${poke.type}</p>
+  </li>
+  `).join('');
+  pokedex.innerHTML = pokemonHTMLString;
+};
